7c590ae98304757dfbe196c2019dbb8168b67c10
Damian <d4m14n36@gmail.com>
2025-04-26 15:42:41 -0300

    created pointer for wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require('wezterm')
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font('VictorMono Nerd Font Mono')
+config.font_size = 14
+config.color_scheme = "Gruvbox Dark"
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+config.enable_scroll_bar =  false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+return config
